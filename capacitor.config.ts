8dacd3ed70efbfead290cd64ce43326046783afe
user <kierhagos0@gmail.com>
2024-04-18 23:34:55 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ACTIVITY-8-HAGOS',
+  webDir: 'www'
+};
+
+export default config;
